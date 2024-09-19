3a9b104762b52a8e4e937234319615f5876e8ccf
YoshihideSogawa <12043975+YoshihideSogawa@users.noreply.github.com>
2024-09-19 16:10:46 +0900

    available

--- flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
+++ flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
@@ -1429,7 +1429,8 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
         }
     }
 
-    open override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, Error?) -> Void)? = nil) {
+    @available(iOS 18.0, *)
+    open override func evaluateJavaScript(_ javaScriptString: String, completionHandler: (@MainActor @Sendable (Any?, (any Error)?) -> Void)? = nil) {
         if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
             if let completionHandler = completionHandler {
                 completionHandler(nil, nil)
@@ -1439,16 +1440,16 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
         super.evaluateJavaScript(javaScriptString, completionHandler: completionHandler)
     }
 
-//     @available(iOS 17.0, *)
-//     public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
-//         if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
-//             if let completionHandler = completionHandler {
-//                 completionHandler(nil, nil)
-//             }
-//             return
-//         }
-//         super.evaluateJavaScript(javaScriptString, completionHandler: completionHandler)
-//     }
+    @unavailable(iOS 18.0)
+    public override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
+        if let applePayAPIEnabled = settings?.applePayAPIEnabled, applePayAPIEnabled {
+            if let completionHandler = completionHandler {
+                completionHandler(nil, nil)
+            }
+            return
+        }
+        super.evaluateJavaScript(javaScriptString, completionHandler: completionHandler)
+    }
     
     @available(iOS 14.0, *)
     public func evaluateJavaScript(_ javaScript: String, frame: WKFrameInfo? = nil, contentWorld: WKContentWorld, completionHandler: ((Result<Any, Error>) -> Void)? = nil) {
